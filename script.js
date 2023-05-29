f24d292918cb5906e3412ccef1b4e74455057f3a
Yel Win Thein <awin4989@gmail.com>
2023-05-29 08:55:46 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,178 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".textDiv", {
+//   duration: 100,
+//   scrollTrigger: {
+//     trigger: ".container",
+//     start: "top top",
+//     end: "bottom 10%",
+//     scrub: 1,
+//     toggleActions: "none none none none",
+
+//     pin: ".textDiv",
+//   },
+// });
+
+gsap.to(".oneMain", {
+  clipPath: "  polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+
+  scrollTrigger: {
+    trigger: ".oneMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+gsap.to(".forTwoImg1", {
+  clipPath: "  polygon(0% 100%, 0% 100%, 0% 100%, 0% 100%)",
+
+  scrollTrigger: {
+    trigger: ".forTwoImg1",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".thirdImg1", {
+  clipPath: "  polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+
+  scrollTrigger: {
+    trigger: ".thirdImg1",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".fourMain", {
+  clipPath: "  polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+
+  scrollTrigger: {
+    trigger: ".fourMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".fiveMain", {
+  clipPath: "  polygon(0% 100%, 0% 100%, 0% 100%, 0% 100%)",
+
+  scrollTrigger: {
+    trigger: ".fiveMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".sixImg", {
+  clipPath: "  polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+
+  scrollTrigger: {
+    trigger: ".sixImg",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".sevenImg", {
+  clipPath: "  polygon(0% 100%, 0% 100%, 0% 100%, 0% 100%)",
+
+  scrollTrigger: {
+    trigger: ".sevenImg",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".eightMain", {
+  clipPath: "  polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+
+  scrollTrigger: {
+    trigger: ".eightMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".nineMain", {
+  clipPath: "  polygon(0% 100%, 0% 100%, 0% 100%, 0% 100%)",
+
+  scrollTrigger: {
+    trigger: ".nineMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".tenMain", {
+  clipPath: "  polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+
+  scrollTrigger: {
+    trigger: ".tenMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+
+gsap.to(".secondFourMain", {
+  clipPath: "  polygon(0% 100%, 0% 100%, 0% 100%, 0% 100%)",
+
+  scrollTrigger: {
+    trigger: ".secondFourMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+gsap.to(".secondFiveMain", {
+  clipPath: "  polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+
+  scrollTrigger: {
+    trigger: ".secondFiveMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+gsap.to(".sixMain", {
+  clipPath: "  polygon(0% 100%, 0% 100%, 0% 100%, 0% 100%)",
+
+  scrollTrigger: {
+    trigger: ".sixMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
+gsap.to(".sevenMain", {
+  clipPath: "  polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)",
+
+  scrollTrigger: {
+    trigger: ".sevenMain",
+    start: "top top",
+    end: "bottom -200px",
+    scrub: 1,
+    toggleActions: "play none none none",
+  },
+});
